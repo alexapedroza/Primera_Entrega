1d39fe0affce48be8c8799e0b56353c31c3dd594
alexapedroza <jpedrozapedraza@gmail.com>
2019-05-15 23:04:40 -0500

    se adiciona primera historia de usuario

--- principal.js
+++ principal.js
@@ -4,10 +4,6 @@ console.log('-------------------------')
 console.log('Cursos disponibles: '+cursos.length)
 console.log('-------------------------')
 
-/*cursos.forEach(element => {
-        print(element)
-});*/
-
 for (i=0; i<cursos.length; i++){
     print(cursos[i], i)
 }
@@ -17,5 +13,5 @@ function print(element, index) {
     {
         console.log('- El nombre del curso es '+element.nombre+ ' con identificador '+element.id 
             +' tiene una duracion de '+ element.duracion + ' horas y un costo de '+ element.valor + ' pesos colombianos.')
-    }, 2000*index);          
+    }, 2000 * index);          
 }
